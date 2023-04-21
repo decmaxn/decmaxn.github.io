2a726d2e3f6e185dedf4685106ae54d852cfd6fc
Victor Ma <victor.ma@gmail.com>
2023-04-21 00:05:29 -0400

    create lambda function and test it

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,11 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
 )
 
 
@@ -13,3 +9,10 @@ class CdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler='hello.handler',
+            code=_lambda.Code.from_asset('lambda'),
+        )
